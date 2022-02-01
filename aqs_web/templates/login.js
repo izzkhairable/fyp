9cdb11b8daf9e29b9a2b56bfe186748b17853605
Jing Wen <ng.jw1599@gmail.com>
2022-02-01 11:30:45 +0800

    login test

--- aqs_web/templates/login.js
+++ aqs_web/templates/login.js
@@ -1,34 +1,38 @@
 function login(){
-    alert("clicked")
 
     var email = document.getElementById("email");
     var password = document.getElementById("password");
-    $(async () => {
-        // Change serviceURL to your own
-        var serviceURL = "http://localhost:5000/login/" + email + password;
-        try {
-            const response =
-                await fetch(
-                    serviceURL, {
-                        method: 'GET'
-                    }
-                );
-            const result = await response.json();
-            if (response.status === 200) {
-                // success case
-                console.log(result)
-            }
-            else if (response.status == 404) {
-                // No Rows
-                console.log(result.message);
-            } else {
-                // unexpected outcome, throw the error
-                throw response.status;
-            }
-        } catch (error) {
-            // Errors when calling the service; such as network error, 
-            // service offline, etc
-            console.log('There is a problem retrieving the data, please try again later.<br />' + error);
-        } // error
-    });
+    console.log("can you see this")
+    console.log(email)
+    console.log(password)
+    
+    // $(async () => {
+    //     // Change serviceURL to your own
+    //     var serviceURL = "http://localhost:5000/login/" + email + password;
+    //     try {
+    //         const response =
+    //             await fetch(
+    //                 serviceURL, {
+    //                     method: 'GET'
+    //                 }
+    //             );
+    //         const result = await response.json();
+    //         if (response.status === 200) {
+    //             // success case
+    //             console.log(result)
+    //             console.log('There is success here')
+    //         }
+    //         else if (response.status == 404) {
+    //             // No Rows
+    //             console.log(result.message);
+    //         } else {
+    //             // unexpected outcome, throw the error
+    //             throw response.status;
+    //         }
+    //     } catch (error) {
+    //         // Errors when calling the service; such as network error, 
+    //         // service offline, etc
+    //         console.log('There is a problem retrieving the data, please try again later.<br />' + error);
+    //     } // error
+    // });
 }
