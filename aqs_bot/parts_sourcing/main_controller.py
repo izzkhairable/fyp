6089db9684d3f0ac323b0a00befc14ff0dfc8b1a
izzkhairable <izzkhair@gmail.com>
2022-02-04 03:31:56 +0800

    Done with quotation downloader & component library comparison

--- aqs_bot/parts_sourcing/main_controller.py
+++ aqs_bot/parts_sourcing/main_controller.py
@@ -1,20 +1,36 @@
 import os
 import sys
+from downloaders.quotation_downloader import quotation_downloader
 
-file_title = "26VSMTC-0012-ST=14.json".replace(".json", "")
+file_titles = quotation_downloader()
+
+
+for file_title in file_titles:
+    os.system(f"python ./scrapers/spiders/google_search_spider.py {file_title}")
+    print("_____________Running Mouser Spider____________")
+    os.system(f"python ./scrapers/spiders/mouser_spider.py {file_title}")
+    print("_____________Running Digikey Spider____________")
+    os.system(f"python ./scrapers/spiders/digikey_spider.py {file_title}")
+    print("_____________Running Element14 Spider____________")
+    os.system(f"python ./scrapers/spiders/element14_spider.py {file_title}")
+    print("_____________Running RsOnline Spider____________")
+    os.system(f"python ./scrapers/spiders/rsonline_spider.py {file_title}")
+    print("_____________Running Misumi Spider____________")
+    os.system(f"python ./scrapers/spiders/misumi_spider.py {file_title}")
+    print("_____________Running Harting Spider____________")
+    os.system(f"python ./scrapers/spiders/harting_spider.py {file_title}")
+    print("_____________Running Schneider Spider____________")
+    os.system(f"python ./scrapers/spiders/schneider_spider.py {file_title}")
+    print("_____________Running Combine Results Function____________")
+    os.system(f"python ./combiners/combine_results.py {file_title}")
+    print("_____________Running  Find Unit Price Quantity Function____________")
+    os.system(f"python ./calculators/find_unit_price_quantity.py {file_title}")
+    print("_____________Running Find Best Supplier Function____________")
+    os.system(f"python ./calculators/find_best_supplier.py {file_title}")
+    print("_____________Running Combine Final Results Function____________")
+    os.system(f"python ./combiners/combine_final_results.py {file_title}")
+    print(f"End of {file_title} part sourcing bot process")
+
+# file_title = "26VSMTC-0012-ST=14.json".replace(".json", "")
 # file_title = "test_bs.json".replace(".json", "")
-os.system(f"python ./scrapers/spiders/google_search_spider.py {file_title}")
-os.system(f"python ./scrapers/spiders/mouser_spider.py {file_title}")
-os.system(f"python ./scrapers/spiders/digikey_spider.py {file_title}")
-os.system(f"python ./scrapers/spiders/element14_spider.py {file_title}")
-os.system(f"python ./scrapers/spiders/rsonline_spider.py {file_title}")
-os.system(f"python ./scrapers/spiders/misumi_spider.py {file_title}")
-os.system(f"python ./scrapers/spiders/harting_spider.py {file_title}")
-os.system(f"python ./scrapers/spiders/schneider_spider.py {file_title}")
-os.system(f"python ./combiners/combine_results.py {file_title}")
-os.system(f"python ./calculators/find_unit_price_quantity.py {file_title}")
-os.system(f"python ./calculators/find_best_supplier.py {file_title}")
-os.system(f"python ./combiners/combine_final_results.py {file_title}")
-
-
 # os.system("python test_spider.py")
