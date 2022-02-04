8514af8ccbc66093b9de17ca8a2dfa0271b594be
darrenho1994 <darkdrium_1994@hotmail.com>
2022-02-04 23:30:54 +0800

    trying out the generate official quotation

---
+++ aqs_bot/quotation_generation/official_quotation.py
@@ -0,0 +1,87 @@
+from docx import Document
+from docx.shared import Inches
+import pyodbc
+import json
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-KMU57HS;'
+                      'Database=myerp101;'
+                      'Trusted_Connection=yes;')
+cursor = conn.cursor()
+
+
+quotation = cursor.execute(
+    "select labour_cost, markup_pct, labour_cost_description from dbo.quotation where quotation_no=?", 'quotation_one')
+res = quotation.fetchone()
+
+markup_pct = None
+if res.markup_pct != None:
+    markup_pct = res.markup_pct/100
+
+
+row_level = cursor.execute(
+    "select row, component_no, description, quantity, uom, unit_price, remark, crawl_info, bom_no from dbo.quotation_component where quotation_no=? and bom_no IS NULL", 'quotation_one')
+result = row_level.fetchall()
+
+bom_list = []
+current_bom = None
+for each_row in result:
+    bom_list.append({'bom': each_row.component_no, 'row':each_row.row, 'qty':each_row.quantity, 'uom':each_row.uom})
+
+
+all_bom = []
+curr_ind = 0
+for bom in bom_list:
+    bom_items = []
+    bom_items.append(bom['bom'])
+    row_level = None
+    curr_bom_row = bom['row']
+    total_price = 0
+    try: 
+        next_bom_row = bom_list[curr_ind+1]['row']
+        row_level = cursor.execute(
+        "select row, component_no, description, quantity, uom, unit_price, remark, crawl_info, bom_no from dbo.quotation_component where quotation_no=? and (row >= ? and row < ?)", 'quotation_one', curr_bom_row, next_bom_row)
+    except:
+         row_level = cursor.execute(
+        "select row, component_no, description, quantity, uom, unit_price, remark, crawl_info, bom_no from dbo.quotation_component where quotation_no=? and row >= ?", 'quotation_one', curr_bom_row)
+       
+    result = row_level.fetchall()
+    for res in result:
+        # print(res.row)
+        
+        if res.bom_no in bom_items:
+            multiple_supplier = False
+
+            bom_items.append(res.component_no)
+
+            if res.crawl_info != None:
+                crawl_info = json.loads(res.crawl_info)
+                if len(crawl_info) > 1:
+                    for ea_supplier in crawl_info:
+                        total_price+= ((ea_supplier['unit_price'] * markup_pct) + ea_supplier['unit_price']) * ea_supplier['qty']
+                    multiple_supplier = True
+            
+            if multiple_supplier == False:
+                total_price+= ((res.unit_price * markup_pct) + res.unit_price) * res.quantity
+
+    all_bom.append({'part_no': bom['bom'], 'qty': bom['qty'], 'uom': bom['uom'], 'unit_price': total_price, 'total_price': total_price/bom['qty']})
+    curr_ind+=1
+
+
+document = Document('aqs_bot/quotation_generation/Official Quotation Template.docx')
+
+# print(document.tables[1].cell(0,1).text)
+
+table = document.tables[2]
+sn = 1
+for ea_bom in all_bom:
+    row = table.add_row().cells#https://github.com/python-openxml/python-docx/issues/205
+    row[0].text = str(sn)
+    row[1].text = str(ea_bom['part_no'])
+    row[3].text = str(ea_bom['qty'])
+    row[4].text = str(ea_bom['uom'])
+    row[5].text = str(ea_bom['unit_price'])
+    row[6].text = str(ea_bom['total_price'])
+    sn+=1
+
+document.save('aqs_bot/quotation_generation/baby.docx')
