82838f500c4e0a3a2d7dc935f87e3740ee116b5b
darrenho1994 <darkdrium_1994@hotmail.com>
2022-02-05 01:06:28 +0800

    minor improvements to code.. now make sure in select statement, sort by row in asc

--- aqs_bot/quotation_generation/excel_quotation.py
+++ aqs_bot/quotation_generation/excel_quotation.py
@@ -31,21 +31,18 @@ markup_pct = None
 if res.markup_pct != None:
     markup_pct = res.markup_pct/100
 
-columns = ['Row', 'Component Number', 'Object Description', 'Qty', 'Component unit', 'Price', 'Total', 'Remarks']
+columns = ['Level', 'Row', 'Component Number', 'Object Description', 'Qty', 'Component unit', 'Price', 'Total', 'Remarks']
+thin = Side(border_style="thin", color="000000")
 
 for i in range(1, len(columns)+1):
     ws.cell(row=1, column=i).value = columns[i-1]
-
-
-thin = Side(border_style="thin", color="000000")
-
-for i in range(1,9):
     ws.cell(row=1, column=i).fill = PatternFill(start_color='4BACC6', end_color='4BACC6', fill_type="solid")
     ws.cell(row=1, column=i).font = Font(bold=True)
     ws.cell(row=1, column=i).border = Border(top=thin, left=thin, right=thin, bottom=thin)
 
+
 row_level = cursor.execute(
-    "select row, component_no, description, quantity, uom, unit_price, remark, crawl_info from dbo.quotation_component where quotation_no=?", 'quotation_one')
+    "select row, lvl, component_no, description, quantity, uom, unit_price, remark, crawl_info from dbo.quotation_component where quotation_no=? ORDER BY row ASC", 'quotation_one')
 result = row_level.fetchall()
 
 
@@ -53,20 +50,21 @@ row_no = 2
 index_no = 1
 for each_row in result:
     multiple_supplier = False
-    qty_location = get_column_letter(4) + str(row_no)
-    unit_price_location = get_column_letter(6) + str(row_no)
+    qty_location = get_column_letter(5) + str(row_no)
+    unit_price_location = get_column_letter(7) + str(row_no)
     
         
     if each_row.crawl_info != None:
         crawl_info = json.loads(each_row.crawl_info)
         if len(crawl_info) > 1:
             for ea_supplier in crawl_info:
-                qty_location = get_column_letter(4) + str(row_no)
-                unit_price_location = get_column_letter(6) + str(row_no)
+                qty_location = get_column_letter(5) + str(row_no)
+                unit_price_location = get_column_letter(7) + str(row_no)
 
                 row_values = [
+                each_row.lvl,
                 index_no, 
-                each_row.component_no, 
+                each_row.component_no,
                 each_row.description, 
                 ea_supplier['qty'], 
                 each_row.uom, 
@@ -85,8 +83,9 @@ for each_row in result:
 
     if multiple_supplier == False:
         row_values = [
+        each_row.lvl,
         index_no, 
-        each_row.component_no, 
+        each_row.component_no,
         each_row.description, 
         each_row.quantity, 
         each_row.uom, 
@@ -102,17 +101,17 @@ for each_row in result:
         row_no+=1
         index_no+=1
 
-start_total_price_location = get_column_letter(7) + str(2)
-end_total_price_location = get_column_letter(7) + str(row_no-1)
+start_total_price_location = get_column_letter(8) + str(2)
+end_total_price_location = get_column_letter(8) + str(row_no-1)
 
 
 row_no+=1
-ws.cell(row=row_no, column=6).value = "LABOUR COST"
-ws.cell(row=row_no, column=7).value = res.labour_cost
-labour_cost_location = get_column_letter(7) + str(row_no)
-ws.cell(row=row_no, column=8).value = res.labour_cost_description
+ws.cell(row=row_no, column=7).value = "LABOUR COST"
+ws.cell(row=row_no, column=8).value = res.labour_cost
+labour_cost_location = get_column_letter(8) + str(row_no)
+ws.cell(row=row_no, column=9).value = res.labour_cost_description
 row_no+=2
-ws.cell(row=row_no, column=6).value = "SELLING PRICE"
-ws.cell(row=row_no, column=7).value = "=SUM(" + start_total_price_location + ":" + end_total_price_location + ")+" + labour_cost_location
+ws.cell(row=row_no, column=7).value = "SELLING PRICE"
+ws.cell(row=row_no, column=8).value = "=SUM(" + start_total_price_location + ":" + end_total_price_location + ")+" + labour_cost_location
 
 wb.save('document.xlsx')
