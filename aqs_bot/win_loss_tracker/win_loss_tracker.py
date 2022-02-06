ecb213d1b312f2ce879f1fba05047be1b0225a14
darrenho1994 <darkdrium_1994@hotmail.com>
2022-02-06 19:40:31 +0800

    I updated the SQL table again.

--- aqs_bot/win_loss_tracker/win_loss_tracker.py
+++ aqs_bot/win_loss_tracker/win_loss_tracker.py
@@ -21,6 +21,22 @@ with open('aqs_bot/win_loss_tracker/config.yaml') as f:
     po_folder = data['po_folder']
     loss_after_x_days = data['loss_after_x_days']
 
+    text_list = []
+
+    po_list = os.listdir(po_folder)
+    for po in po_list:
+        with pdfplumber.open(po_folder + '\\' + po) as pdf:
+            page = pdf.pages[0]
+            text = page.extract_text()
+        if len(text) == 0:
+            print("Performing OCR.")
+            os.system(f'ocrmypdf "' + po_folder + '\\' + po + '" "' + po_folder + '\\temp.pdf' + '"')
+            with pdfplumber.open(po_folder + '\\temp.pdf') as pdf:
+                page = pdf.pages[0]
+                text = page.extract_text()
+            os.remove(po_folder + '\\temp.pdf')
+        text_list.append(text)
+
     for quotation in result:
         rfq_split = quotation.quotation_no.split('-')
         rfq_no_without_rev = "-".join(rfq_split[0:len(rfq_split)-1])
@@ -28,22 +44,8 @@ with open('aqs_bot/win_loss_tracker/config.yaml') as f:
         no_of_days_since_rfq_received = (datetime.datetime.today() - quotation.rfq_date).days
 
     #https://ocrmypdf.readthedocs.io/en/latest/installation.html#installing-on-windows
-        
-        po_list = os.listdir(po_folder)
-        
-        for po in po_list:
-            with pdfplumber.open(po_folder + '\\' + po) as pdf:
-                page = pdf.pages[0]
-                text = page.extract_text()
-            if len(text) == 0:
-                print("Performing OCR.")
-                os.system(f'ocrmypdf "' + po_folder + '\\' + po + '" "' + po_folder + '\\temp.pdf' + '"')
-                with pdfplumber.open(po_folder + '\\temp.pdf') as pdf:
-                    page = pdf.pages[0]
-                    text = page.extract_text()
-                os.remove(po_folder + '\\temp.pdf')
-
-            if rfq_no_without_rev in text:
+        for each_text in text_list:
+            if rfq_no_without_rev in each_text:
                 print("Win: " + quotation.quotation_no)
                 cursor.execute("update dbo.quotation set status=? where quotation_no=?", 'win', quotation.quotation_no)
                 conn.commit()
@@ -52,7 +54,8 @@ with open('aqs_bot/win_loss_tracker/config.yaml') as f:
             elif no_of_days_since_rfq_received > loss_after_x_days:
                 print("Lose: " + quotation.quotation_no)
                 cursor.execute("update dbo.quotation set status=? where quotation_no=?", 'lose', quotation.quotation_no)
-                conn.commit()
+                conn.commit()        
+                    
 conn.close()
 
 '''
--- how_to_delete_boms.py
+++ how_to_delete_boms.py
@@ -1,4 +1,6 @@
 import pyodbc
+import time
+start_time = time.time()
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=DESKTOP-KMU57HS;'
@@ -7,23 +9,37 @@ conn = pyodbc.connect('Driver={SQL Server};'
 cursor = conn.cursor()
 
 ## DELETE ITEM 4...
-cursor.execute("DELETE FROM dbo.quotation_component WHERE quotation_no=? AND component_no=?", 'quotation_one', 'bom_a')
+cursor.execute("DELETE FROM dbo.quotation_component WHERE quotation_no=? AND component_no=?", 'rfqsample', '26VS691A-01.8')
 conn.commit()
 
 
 ## ONCE ITEM 4 IS DELETED, ADD IT TO THE ARRAY
-deleted_components = ['bom_a']
+deleted_components = ['26VS691A-01.8']
 
 ## RETRIEVE ALL COMPONENTS FOR THE CURRENT QUOTATION
 row_level = cursor.execute(
-    "select row, component_no, description, quantity, uom, unit_price, remark, crawl_info, bom_no from dbo.quotation_component where quotation_no=? ORDER BY ROW ASC", 'quotation_one')
+    "select row, component_no, description, quantity, uom, unit_price, remark, crawl_info, bom_no from dbo.quotation_component where quotation_no=? ORDER BY ROW ASC", 'rfqsample')
 result = row_level.fetchall()
 
 ## LOOP THROUGH EACH COMPONENTS, CHECK IF BOM_NO IS IN THE DELETED_COMPONENTS LIST. IF IT IS FOUND. IT MEANS IT  BELONGS TO THE BOM WE JUST DELETED, THEREFORE WE DELETE THIS COMPONENT TOO :)
 for i in result:
     if i.bom_no in deleted_components:
         deleted_components.append(i.component_no)
-        cursor.execute("DELETE FROM dbo.quotation_component WHERE quotation_no=? AND component_no=?", 'quotation_one', i.component_no)
+        cursor.execute("DELETE FROM dbo.quotation_component WHERE quotation_no=? AND component_no=?", 'rfqsample', i.component_no)
         conn.commit()
 
-print("ok")
+
+'''Now fix the row numberin '''
+row_level = cursor.execute(
+    "select row, quotation_no, component_no, lvl, uom, description, quantity, unit_price, is_bom, bom_no, remark, crawl_info from dbo.quotation_component where quotation_no=? ORDER BY ROW ASC", 'rfqsample')
+result = row_level.fetchall()
+
+row_no = 1
+for i in result:
+    cursor.execute("DELETE FROM dbo.quotation_component WHERE quotation_no=? AND row=?", 'rfqsample', i.row)
+    conn.commit()
+    cursor.execute("INSERT INTO dbo.quotation_component (row, quotation_no, component_no, lvl, uom, description, quantity, unit_price, is_bom, bom_no, remark, crawl_info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+    row_no, i.quotation_no, i.component_no, i.lvl, i.uom, i.description, i.quantity, i.unit_price, i.is_bom, i.bom_no, i.remark, i.crawl_info)
+    conn.commit()
+    row_no+=1
+print("--- %s seconds ---" % (time.time() - start_time))
