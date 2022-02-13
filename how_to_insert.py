30842f556aa9689569a504bcde7235371b3f1c22
darrenho1994 <darkdrium_1994@hotmail.com>
2022-02-13 15:07:21 +0800

    how to insert code

--- how_to_insert.py
+++ how_to_insert.py
@@ -4,36 +4,46 @@ import math
 start_time = time.time()
 
 conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DESKTOP-KMU57HS;'
+                      'Server=DESKTOP-KMU57HS\SQLEXPRESS;'
                       'Database=myerp101;'
                       'Trusted_Connection=yes;')
 cursor = conn.cursor()
 
 
-get_bom = cursor.execute("SELECT row, id, lvl FROM dbo.quotation_component WHERE quotation_no=? AND id=?", 'quotation_one', 4)
+get_bom = cursor.execute("SELECT row, id, lvl FROM dbo.quotation_component WHERE id=?", 2)
 result = get_bom.fetchone()
-
 bom_id = result.id
 bom_lvl = math.ceil(result.lvl)
 bom_row = result.row
-insert_to = bom_row+1
+component_level_to_insert = bom_lvl+1
+
+get_range = cursor.execute("SELECT row, id, lvl FROM dbo.quotation_component WHERE quotation_no=? AND row >= ? ORDER BY ROW", 'quotation_one', bom_row)
+result = get_range.fetchall()
 
 
-
-'''Now fix the row numberin '''
-row_level = cursor.execute(
-    "select id, row, quotation_no, component_no, lvl, uom, description, quantity, unit_price, is_bom, bom_id, remark, crawl_info from dbo.quotation_component where quotation_no=? and row >= ? ORDER BY ROW ASC", 'quotation_one', insert_to)
-result = row_level.fetchall()
-row_no = insert_to+1
-for i in result:
-    cursor.execute("UPDATE dbo.quotation_component SET row=? WHERE quotation_no=? AND id=?", row_no, 'quotation_one', i.id)
-    conn.commit()
-    row_no+=1
-print("--- %s seconds ---" % (time.time() - start_time))
-
+row_to_insert_new_item = None
+for each_row in result:
+    next_item = cursor.execute("SELECT row, id, lvl FROM dbo.quotation_component WHERE quotation_no=? AND row = ? ORDER BY ROW", 'quotation_one', each_row.row+1)
+    res = get_range.fetchone()
+    if res != None:
+        if component_level_to_insert > res.lvl:
+            row_to_insert_new_item = each_row.row+1
+            break        
+    else:
+        row_to_insert_new_item = each_row.row+1
 
 
-'''NOW INSERT!'''
-cursor.execute("insert into dbo.quotation_component(row, quotation_no, component_no, lvl, uom, description, quantity, unit_price, is_bom, bom_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-insert_to, 'quotation_one', 'new_inserted_component', bom_lvl+1, 'EA', 'new component description', '1', None, 0, bom_id)
+## Fix the rows before inserting ##
+get_range = cursor.execute("SELECT row, id, lvl FROM dbo.quotation_component WHERE quotation_no=? AND row >= ? ORDER BY ROW", 'quotation_one', row_to_insert_new_item)
+result = get_range.fetchall()
+
+if len(result) > 0:
+    for each_row in result:
+        cursor.execute("UPDATE dbo.quotation_component SET row=? WHERE quotation_no=? AND id=?", each_row.row+1, 'quotation_one', each_row.id)
+        conn.commit()
+
+
+## Now let's insert the new item ##
+cursor.execute("INSERT INTO dbo.quotation_component (quotation_no, row, component_no, lvl, uom, description, quantity, unit_price, is_bom, bom_id, remark, crawl_info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 'quotation_one', row_to_insert_new_item, 'New component', component_level_to_insert, 'EA', 'New component description', 69, 1.50, 0, bom_id, 'no remarks', None)
 conn.commit()
+
